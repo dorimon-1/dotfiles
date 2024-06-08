dbbff342b3cc47395515d8672c51a0fe0e650ce2
dor4420 <dorsahar@icloud.com>
2024-06-08 21:32:00 +0300

    Disable codelens

--- nvim/lua/custom/plugins/configs/lspconfig.lua
+++ nvim/lua/custom/plugins/configs/lspconfig.lua
@@ -1,13 +1,17 @@
+local lspconfig = require 'lspconfig'
+local util = require 'lspconfig/util'
 
-local lspconfig = require("lspconfig")
-local util = require "lspconfig/util"
-
+-- Setup gopls
 lspconfig.gopls.setup {
-  cmd = {"gopls"},
-  filetypes = { "go", "gomod", "gowork", "gotmpl" },
-  root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+  cmd = { 'gopls' },
+  filetypes = { 'go', 'gomod', 'gowork', 'gotmpl' },
+  root_dir = util.root_pattern('go.work', 'go.mod', '.git'),
   settings = {
     gopls = {
+      codelenses = {
+        generate = false,
+        test = false,
+      },
       completeUnimported = true,
       usePlaceholders = true,
       analyses = {
@@ -15,8 +19,33 @@ lspconfig.gopls.setup {
       },
     },
   },
+  on_attach = function(client, bufnr)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+    -- Mappings.
+    local opts = { noremap = true, silent = true }
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    -- Add other key mappings as needed
+  end,
+  flags = {
+    debounce_text_changes = 150,
+  },
 }
 
-lspconfig.pyright.setup({
-  filetypes = {"python"},
-})
+-- Setup pyright
+lspconfig.pyright.setup {
+  filetypes = { 'python' },
+  on_attach = function(client, bufnr)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+    -- Mappings.
+    local opts = { noremap = true, silent = true }
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    -- Add other key mappings as needed
+  end,
+  flags = {
+    debounce_text_changes = 150,
+  },
+}
