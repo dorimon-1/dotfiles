9d797eaf9a160d5083728a5b3cfefad60409510c
dor4420 <dorsahar@icloud.com>
2024-07-25 21:26:04 +0300

    Update which-key to v.3

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -3,27 +3,27 @@ local M = {}
 M.general = {
   n = {
     -- close buffer
-    ['<leader>x'] = { '<cmd> bd <CR>', 'Close buffer' },
+    { '<leader>x', '<cmd> bd <CR>', desc = 'Close buffer' },
   },
 }
 
 M.filetree = {
   n = {
     -- toggle
-    ['<C-n>'] = { '<cmd> Neotree toggle<CR>', 'Toggle nvimtree' },
+    { '<C-n>',     '<cmd> Neotree toggle<CR>', desc = 'Toggle nvimtree' },
 
     -- focus
-    ['<leader>e'] = { '<cmd> Neotree focus<CR>', 'Open file browser' },
+    { '<leader>e', '<cmd> Neotree focus<CR>',  desc = 'Open file browser' },
   },
 }
 
 M.dap = {
   n = {
-    ['<leader>dg'] = { '<cmd> DapContinue<CR>', 'Debug: Start/Continue' },
-    ['<leader>dsi'] = { '<cmd> DapStepInto<CR>', 'Debug: Step Into' },
-    ['<leader>dso'] = { '<cmd> DapStepOver<CR>', 'Debug: Step Over' },
-    ['<leader>dse'] = { '<cmd> DapStepOut<CR>', 'Debug: Step Out' },
-    ['<leader>b'] = { '<cmd> DapToggleBreakpoint<CR>', 'Debug: Toggle Breakpoint' },
+    { '<leader>dg',  '<cmd> DapContinue<CR>',         desc = 'Debug: Start/Continue' },
+    { '<leader>dsi', '<cmd> DapStepInto<CR>',         desc = 'Debug: Step Into' },
+    { '<leader>dso', '<cmd> DapStepOver<CR>',         desc = 'Debug: Step Over' },
+    { '<leader>dse', '<cmd> DapStepOut<CR>',          desc = 'Debug: Step Out' },
+    { '<leader>b',   '<cmd> DapToggleBreakpoint<CR>', desc = 'Debug: Toggle Breakpoint' },
     -- ['<leader>B'] = {
     --   function()
     --     dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
@@ -35,72 +35,74 @@ M.dap = {
 
 M.harpoon = {
   n = {
-    ['<leader>hh'] = { ':lua require("harpoon.ui").toggle_quick_menu()<CR>', 'Harpoon file' },
-    ['<leader>ha'] = { ':lua require("harpoon.mark").add_file()<CR>', 'Harpoon file' },
-    ['<leader>hn'] = { ':lua require("harpoon.ui").nav_next()<CR>', 'Navigate to next harpooned file' },
-    ['<leader>hb'] = { ':lua require("harpoon.ui").nav_prev()<CR>', 'Navigate to previous harpooned file' },
+    { '<leader>hh', ':lua require("harpoon.ui").toggle_quick_menu()<CR>', desc = 'Harpoon file' },
+    { '<leader>ha', ':lua require("harpoon.mark").add_file()<CR>',        desc = 'Harpoon file' },
+    { '<leader>hn', ':lua require("harpoon.ui").nav_next()<CR>',          desc = 'Navigate to next harpooned file' },
+    { '<leader>hb', ':lua require("harpoon.ui").nav_prev()<CR>',          desc = 'Navigate to previous harpooned file' },
   },
 }
 
 M.refactoring = {
   x = {
     -- Extract
-    ['<leader>re'] = { ':Refactor extract ', 'Extract' },
-    -- Extract to file
-    ['<leader>rf'] = { ':Refactor extract_to_file ', 'Extract to file' },
-    -- Extract variable
-    ['<leader>rv'] = { ':Refactor extract_var ', 'Extract variable' },
+    { '<leader>re', ':Refactor extract ',         desc = 'Extract' },
+    --- Extract to file
+    { '<leader>rf', ':Refactor extract_to_file ', desc = 'Extract to file' },
+    --- Extract variable
+    { '<leader>rv', ':Refactor extract_var ',     desc = 'Extract variable' },
   },
   n = {
+
     -- Inline variable
-    ['<leader>ri'] = { ':Refactor inline_var', 'Inline variable' },
+    { '<leader>ri',  ':Refactor inline_var',            desc = 'Inline variable' },
     -- Inline function
-    ['<leader>rI'] = { ':Refactor inline_func', 'Inline function' },
+    { '<leader>rI',  ':Refactor inline_func',           desc = 'Inline function' },
+
     -- Extract block
-    ['<leader>rb'] = { ':Refactor extract_block', 'Extract block' },
+    { '<leader>rb',  ':Refactor extract_block',         desc = 'Extract block' },
     -- Extract block to file
-    ['<leader>rbf'] = { ':Refactor extract_block_to_file', 'Extract block to file' },
+    { '<leader>rbf', ':Refactor extract_block_to_file', desc = 'Extract block to file' },
   },
 }
 
 M.bufferline = {
   n = {
     -- Cycle to the next buffer
-    ['<Tab>'] = { ':BufferLineCycleNext<CR>', 'Next buffer' },
+    { '<Tab>',   ':BufferLineCycleNext<CR>', desc = 'Next buffer' },
     -- Cycle to the previous buffer
-    ['<S-Tab>'] = { ':BufferLineCyclePrev<CR>', 'Previous buffer' },
+    { '<S-Tab>', ':BufferLineCyclePrev<CR>', desc = 'Previous buffer' },
   },
 }
 
 M.telescope = {
   n = {
     -- find
-    ['<leader>ff'] = { '<cmd> Telescope find_files <CR>', 'Find files' },
-    ['<leader>fa'] = { '<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>', 'Find all' },
-    ['<leader>fb'] = { '<cmd> Telescope buffers <CR>', 'Find buffers' },
-    ['<leader>fh'] = { '<cmd> Telescope help_tags <CR>', 'Help page' },
-    ['<leader>fo'] = { '<cmd> Telescope oldfiles <CR>', 'Find oldfiles' },
-    ['<leader>fz'] = { '<cmd> Telescope current_buffer_fuzzy_find <CR>', 'Find in current buffer' },
-    ['<leader>fe'] = { '<cmd> Telescope file_browser<CR>', 'Open file browser' },
+    { '<leader>ff', '<cmd> Telescope find_files <CR>',                                        desc = 'Find files' },
+    { '<leader>fa', '<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>', desc = 'Find all' },
+    { '<leader>fb', '<cmd> Telescope buffers <CR>',                                           desc = 'Find buffers' },
+    { '<leader>fh', '<cmd> Telescope help_tags <CR>',                                         desc = 'Help page' },
+    { '<leader>fo', '<cmd> Telescope oldfiles <CR>',                                          desc = 'Find oldfiles' },
+    { '<leader>fz', '<cmd> Telescope current_buffer_fuzzy_find <CR>',                         desc = 'Find in current buffer' },
+    { '<leader>fe', '<cmd> Telescope file_browser<CR>',                                       desc = 'Open file browser' },
     -- git
-    ['<leader>cm'] = { '<cmd> Telescope git_commits <CR>', 'Git commits' },
-    ['<leader>gt'] = { '<cmd> Telescope git_status <CR>', 'Git status' },
+    { '<leader>cm', '<cmd> Telescope git_commits <CR>',                                       desc = 'Git commits' },
+    { '<leader>gt', '<cmd> Telescope git_status <CR>',                                        desc = 'Git status' },
 
     -- pick a hidden term
-    ['<leader>pt'] = { '<cmd> Telescope terms <CR>', 'Pick hidden term' },
+    { '<leader>pt', '<cmd> Telescope terms <CR>',                                             desc = 'Pick hidden term' },
 
-    -- theme switcher
-    ['<leader>th'] = { '<cmd> Telescope themes <CR>', 'Nvchad themes' },
+    --- theme swit theme switcher
+    { '<leader>th', '<cmd> Telescope themes <CR>',                                            desc = 'Nvchad themes' },
 
-    ['<leader>ma'] = { '<cmd> Telescope marks <CR>', 'telescope bookmarks' },
-    ['<leader>fw'] = { '<cmd> Telescope live_grep <CR>', 'Live Grep' },
+    { '<leader>ma', '<cmd> Telescope marks <CR>',                                             desc = 'telescope bookmarks' },
+    { '<leader>fw', '<cmd> Telescope live_grep <CR>',                                         desc = 'Live Grep' },
   },
 }
 
 M.lazygit = {
   n = {
     -- Lazygit
-    ['<leader>gg'] = { '<cmd> LazyGit <CR>', 'LazyGit' },
+    { '<leader>gg', '<cmd> LazyGit <CR>', desc = 'LazyGit' },
   },
 }
 
